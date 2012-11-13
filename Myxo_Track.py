74f0b9c7f454d62d5949645e8c0c426a5fe115e7
Leon Espinosa <leonespcast@gmail.com>
2012-11-13 10:41:49 +0100

    Correction de tous les path vers MorphoBact2

--- Myxo_Track.py
+++ Myxo_Track.py
@@ -23,9 +23,8 @@ import random
 
 username=getpass.getuser()
 
-mypath=os.path.expanduser(IJ.getDirectory("plugins")+"MeasureCells")
-#mypath=os.path.expanduser("~/Dropbox/MacrosDropBox/py/MeasureCells_7")
-#mypath=os.path.expanduser(os.path.join("~","Dropbox","MacrosDropBox","py","MeasureCells_7"))
+#mypath=os.path.expanduser(IJ.getDirectory("plugins")+"MeasureCells")
+mypath=os.path.expanduser(os.path.join("~","Dropbox","MacrosDropBox","py","MorphoBact2"))
 sys.path.append(mypath)
 
 from org.python.core import codecs
